4a4e9c8a339d46038186b974e5499b599d0b7698
joshua <josh.d.armour@gmail.com>
2023-08-03 20:00:16 +1000

    Get rid of tests and added database migrations

--- SetUp.cs
+++ SetUp.cs
@@ -9,19 +9,16 @@ public class SetUp
     public void ConfigureServices(IServiceCollection services)
     {
         string connectionString = "Data Source=data.sqlite";
-        var database = new Shop_Manager.Database.Database();
-        database.createTables();
-
-        services.AddDbContext<Shop_Manager.DbContext.RecordsDbContext>(
+        services.AddDbContext<DbContext.ShopDbContext>(
             options => options.UseSqlite(connectionString)
         );
+
+        services.AddScoped<IShopDbContext, DbContext.ShopDbContext>();
         
-        services.AddScoped<Shop_Manager.DbContext.IRecordsDbContext, Shop_Manager.DbContext.RecordsDbContext>();
-        services.AddScoped<Shop_Manager.Services.RecordsServices>();
-    }
-
-    public void setUpTests(IServiceCollection services)
-    {
-
+        services.AddScoped<Services.RecordServices>();
+        services.AddScoped<Services.DriverTripServices>();
+        services.AddScoped<Services.EmployeeServices>();
+        services.AddScoped<Services.EmployeeShiftServices>();
+        services.AddScoped<Services.RegionServices>();
     }
 }
